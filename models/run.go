70eaecace25e5d71cac986ba3f7f631ea25cd7a1
Max Krivich <maxkrivich@gmail.com>
2018-02-06 16:43:11 +0200

    Fix soft delete and add model validator

--- models/run.go
+++ models/run.go
@@ -8,6 +8,16 @@ type Run struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
-	deletedAt *time.Time `sql:"index"`
-	Name      string     `gorm:"size:255; not null" json:"name" binding:"required"`
+	DeletedAt *time.Time `sql:"index" json:"-"`
+	Name      string     `gorm:"size:255; not null" json:"name"`
+}
+
+func (r Run) Validate() bool {
+	if len(r.Name) == 0 {
+		return false
+	}
+	if r.ID != 0 || r.UpdatedAt != (time.Time{}) || r.CreatedAt != (time.Time{}) || r.DeletedAt != nil {
+		return false
+	}
+	return true
 }
